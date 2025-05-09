1bc727cfc2046e9da350bf04656d6fd866c65982
Paulo Rodrigues <62611167+vcrpaulo00@users.noreply.github.com>
2025-05-09 16:41:16 -0300

    Add files via upload
    Mathematical challenge in JavaScript

---
+++ desafio-2.js
@@ -0,0 +1,7 @@
+let raio = 20
+const PI = 3.14 // deixar como const (declarar em capslock)
+
+// let area = pi * (raio**2)
+const area = PI * Math.pow(raio, 2) // pow ou power (elevado), facilita na elevação de cálculos matemáticos
+
+console.log('Área da circunferência é', area)
